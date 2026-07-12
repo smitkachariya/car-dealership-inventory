cccfab7ef00bb2208a388fe9462d8c939ae05a71
Smit <smitkachariya128@gmail.com>
2026-07-12 17:15:56 +0530

    feat: enables the cors

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,30 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
-const app = express();
+const connectDB = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
 const vehicleRoutes = require("./routes/vehicleRoutes");
+
+const app = express();
+
+connectDB();
+
+/* Middleware */
+app.use(cors());
 app.use(express.json());
-app.use("/api/auth", authRoutes);
+
+/* Routes */
 app.get("/", (req, res) => {
   res.send("Car Dealership Inventory API is running...");
 });
+
+app.use("/api/auth", authRoutes);
 app.use("/api/vehicles", vehicleRoutes);
 
 const PORT = process.env.PORT || 5000;
-require("dotenv").config();
-const connectDB = require("./config/db");
-connectDB();
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
